6b2eb7920ad2697a134662b3aa19f296645372ca
aljo <johansson_a95@hotmail.com>
2024-03-30 19:15:17 +0100

    initial - background service worker

--- background.js
+++ background.js
@@ -1,8 +1,7 @@
 chrome.tabs.onUpdated.addListener(
     function(tabId, changeInfo) {
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-            if ((changeInfo.url) && (changeInfo.url).substring(0, 30) === "https://www.youtube.com/watch?") {
-                console.log("sending?")
+            if ((changeInfo.url) && ((changeInfo.url).substring(0, 30) === "https://www.youtube.com/watch?")) {
                 chrome.tabs.sendMessage(tabs[0].id, {action: "placeholder", url: changeInfo.url}); 
             } 
         });
