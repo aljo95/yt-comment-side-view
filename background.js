7628d48ce04703e388fe3b2b98175eac88730f9e
aljo <johansson_a95@hotmail.com>
2024-04-03 23:46:58 +0200

    manual injector for non-video pages

--- background.js
+++ background.js
@@ -1,9 +1,17 @@
+
 chrome.tabs.onUpdated.addListener(
     function(tabId, changeInfo) {
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-            if ((changeInfo.url) && ((changeInfo.url).substring(0, 30) === "https://www.youtube.com/watch?")) {
-                chrome.tabs.sendMessage(tabs[0].id, {action: "placeholder", url: changeInfo.url}); 
+            let hasBeenRun = false
+            chrome.runtime.onMessage.addListener(
+                function(req) {
+                    if (req.msg === "init")
+                        hasBeenRun = true    
+                }
+            )
+            if ((changeInfo.url) && ((changeInfo.url).substring(0, 30) === "https://www.youtube.com/watch?") && (!hasBeenRun)) {
+                chrome.scripting.executeScript({target: {tabId: tabId}, files: ['scripts/content.js']});
             } 
         });
-    }
+    } 
 );
